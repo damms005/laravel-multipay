0d0f8d94281d60cb94809c61b5401910056aa3d5
Damilola Olowookere <damms005@gmail.com>
2021-09-25 10:08:10 +0100

    Update README

--- views/test-drive/pay.blade.php
+++ views/test-drive/pay.blade.php
@@ -1,19 +1,44 @@
 <form action="{{ route('payment.show_transaction_details_for_user_confirmation') }}" method="post">
-	<!-- Any of the handlers listed in the Supported Payment Handlers section of this README -->
-	Payment handler. (Good UX tip 👍: this should be a hidden field)
-	<input type="text" name="payment_processor" value="Paystack" />
+	<div>
+		<!-- Any of the handlers listed in the Supported Payment Handlers section of this README -->
+		Payment handler (Good UX tip 👍: this should be a hidden field)
+		<div>
+			<input type="text" name="payment_processor" value="Paystack" />
+		</div>
+	</div>
 	<br>
 
-	<input type="text" name="amount" value="12345" />
+	<div>
+		Amount
+		<div>
+			<input type="text" name="amount" value="12345" />
+		</div>
+	</div>
 	<br>
 
-	<!-- ISO-4217 format. Ensure to check that the payment handler you specified above supports this currency -->
-	<input type="text" name="currency" value="NGN" />
+	<div>
+		Currency (ISO-4217 format. Ensure to check that the payment handler you specified above supports this currency)
+		<div>
+			<input type="text" name="currency" value="NGN" />
+		</div>
+	</div>
 	<br>
 
-	<!-- id of the user making the payment -->
-	<input type="number" name="user_id" value="1" />
+	<div>
+		ID of the user making the payment. Typically, this is an Eloquent authenticated user model
+		<div>
+			<input type="number" name="user_id" value="1" />
+		</div>
+	</div>
 	<br>
 
-	<input type="text" name="transaction_description" value="Payment for Tesla Model Y picture" />
+	<div>
+		Description
+		<div>
+			<input type="text" name="transaction_description" value="Payment for Tesla Model Y picture" />
+		</div>
+	</div>
+	<br>
+
+	<button type="submit"></button>
 </form>
