feb1de0c9569a18d3cfcf322dd44000881591f1d
Damilola Olowookere <damms005@gmail.com>
2021-09-25 10:03:39 +0100

    Remove auth middleware from test-drive route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ Route::group([
 
 	//take it for a spin
 	Route::get('/test-drive', function () {
-		return view('laravel-cashier:test-drive.pay');
+		return view('laravel-cashier::test-drive.pay');
 	})->name('payment.test-drive');
 });
