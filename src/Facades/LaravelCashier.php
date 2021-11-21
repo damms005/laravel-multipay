052d7dfec3c9f9372608c1f2130b9f2d713d0948
Damilola Olowookere <damms005@gmail.com>
2021-11-21 19:52:35 +0100

    Make Facade comply to PSR

--- src/Facades/LaravelCashier.php
+++ src/Facades/LaravelCashier.php
@@ -4,7 +4,7 @@ namespace Damms005\LaravelCashier\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class LaravelCashierFacade extends Facade
+class LaravelCashier extends Facade
 {
     protected static function getFacadeAccessor()
     {
