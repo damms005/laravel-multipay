76a0fcd55745a89419dbac10f49e7bc361e97a90
Damilola Olowookere <damms005@gmail.com>
2023-07-25 01:29:16 +0000

    Set 'name' field as unique in PaymentPlan table

--- database/migrations/2023_07_24_074517_subscription_service.php
+++ database/migrations/2023_07_24_074517_subscription_service.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
     {
         Schema::create((new PaymentPlan())->getTable(), function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('amount');
             $table->string('interval');
             $table->string('description');
