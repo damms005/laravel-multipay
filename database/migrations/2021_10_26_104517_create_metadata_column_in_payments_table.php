2ed7465faae55946f08633e3d002f392acc18657
Damilola Olowookere <damms005@gmail.com>
2023-07-25 01:36:53 +0000

    Refactor migration classes to anonymous classes

--- database/migrations/2021_10_26_104517_create_metadata_column_in_payments_table.php
+++ database/migrations/2021_10_26_104517_create_metadata_column_in_payments_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMetadataColumnInPaymentsTable extends Migration
+return new class() extends Migration
 {
     public function up()
     {
@@ -27,4 +27,4 @@ class CreateMetadataColumnInPaymentsTable extends Migration
             $table->dropColumn("metadata");
         });
     }
-}
+};
