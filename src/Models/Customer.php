e284ac0ca513ad75b21a2d4e1ce666cd7a2d013e
Damilola Olowookere <damms005@gmail.com>
2023-08-19 17:06:33 +0000

    Add payment customers table and update webhooks

---
+++ src/Models/Customer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Damms005\LaravelMultipay\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Customer extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+}
