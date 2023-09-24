3bdd97e4a660a3779f63d8c4dce007adffe69137
Damilola Olowookere <damms005@gmail.com>
2023-09-24 16:08:15 +0000

    Add metadata column to subscriptions table

--- database/migrations/2023_09_24_010000_add_metadata_to_subscriptions.php
+++ database/migrations/2023_09_24_010000_add_metadata_to_subscriptions.php
@@ -1,6 +1,6 @@
 <?php
 
-use Damms005\LaravelMultipay\Models\PaymentPlan;
+use Damms005\LaravelMultipay\Models\Subscription;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Schema;
 return new class () extends Migration {
     public function up()
     {
-        Schema::table((new PaymentPlan())->getTable(), function (Blueprint $table) {
+        Schema::table((new Subscription())->getTable(), function (Blueprint $table) {
             $table->json('metadata')->nullable()->after('next_payment_due_date');
         });
     }
