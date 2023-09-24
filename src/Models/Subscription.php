e41a146f82af29432b5c88f2aa8f874e42629429
Damilola Olowookere <damms005@gmail.com>
2023-09-24 19:43:10 +0000

    Add metadata cast to Subscription model

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -4,6 +4,7 @@ namespace Damms005\LaravelMultipay\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 
 class Subscription extends Model
 {
@@ -15,6 +16,7 @@ class Subscription extends Model
 
     protected $casts = [
         'next_payment_due_date' => 'datetime',
+        'metadata' => AsArrayObject::class,
     ];
 
     public function getTable(): string
