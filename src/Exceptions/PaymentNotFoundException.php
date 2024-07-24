5ef8c1a51d9daf01a3d08e5286ad0539ff0a2bd8
Damilola Olowookere <damms005@gmail.com>
2024-07-24 10:59:20 +0100

    Refactor PaymentNotFoundException constructor parameters

--- src/Exceptions/PaymentNotFoundException.php
+++ src/Exceptions/PaymentNotFoundException.php
@@ -6,4 +6,12 @@ use Exception;
 
 class PaymentNotFoundException extends Exception
 {
+    public function __construct(
+        public \Illuminate\Http\Request $webhookRequest,
+        ?string $message = null,
+    ) {
+        $message = $message ?? 'Payment not found in Paystack\'s charge.success event. Payload: ' . json_encode($webhookRequest->all());
+
+        parent::__construct($message);
+    }
 }
